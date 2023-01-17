1e20a079ca2b7e5443b92c6656a3730a1624de07
MicoBledsoe <MicoMods@gmail.com>
2023-01-17 18:16:45 +0000

    commit

--- 0x03-python-more_data_structures/2-uniq_add.py
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    total = 0
-    for i in set(my_list):
-         total += i
-    return(total)
-
-# return (sum(set(my_list)))
+    return sum(set(my_list))
