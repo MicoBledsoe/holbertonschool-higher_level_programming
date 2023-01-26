1aa2ce76bae2cc1c9ba4181fde0e6a2ca00fa8b9
MicoBledsoe <MicoMods@gmail.com>
2023-01-26 08:02:48 +0000

    removed vars func and replaced with lookup func

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-return vars(obj)
+""" defines a function "lookup" which takes an object and returns a 
+list of the object's attributes and methods using the built-in dir function."""
+    return dir(obj)
