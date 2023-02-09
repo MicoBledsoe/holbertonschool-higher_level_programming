5b667bdec598ec1eec0ee44cbb3b4f59a34e1708
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-09 13:30:06 -0600

    Update 15-groups.sql

--- 0x00-SQL_introduction/15-groups.sql
+++ 0x00-SQL_introduction/15-groups.sql
@@ -1,10 +1,5 @@
 -- selects the number of records with same score in table
-
-<<<<<<< HEAD
 SELECT score, COUNT(*) as number
-=======
-SELECT score, COUNT(score) as count
->>>>>>> 3aa248e06c1f8b675087484ceae26883b2891f06
 FROM second_table
 GROUP BY score
-GROUP BY number DESC;
+GROUP BY as number DESC;
