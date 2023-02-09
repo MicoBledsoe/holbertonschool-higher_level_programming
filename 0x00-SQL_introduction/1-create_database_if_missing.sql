f92bb0508ff364c23d4758f44f35fb5dea970517
MicoBledsoe <MicoMods@gmail.com>
2023-02-08 23:07:20 -0800

    added[IF NOT EXIST] to sure my code will not fail if the database happen to already exists

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- creates a database
-CREATE DATABASE hbtn_0c_0;
+-- creates a database and also makes sure the script will not break if the database already exists
+CREATE IF NOT EXISTS DATABASE hbtn_0c_0;
