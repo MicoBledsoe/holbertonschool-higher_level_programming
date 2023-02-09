3d9a2dc4e21002e5a96eaeca4e3dd8e627febf0d
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-09 03:00:25 -0600

    Update 9-full_creation.sql

--- 0x00-SQL_introduction/9-full_creation.sql
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creating a 2nd table in database and also store data to tables
-CREATE TABLE IF NOT EXIST second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 id INT,
 name VARCHAR(256),
 score INT
