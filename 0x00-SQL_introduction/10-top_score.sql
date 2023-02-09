47a36c7b3d4d58dadb22a5b890a84e0795bcc824
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-09 11:45:00 -0600

    Update 10-top_score.sql

--- 0x00-SQL_introduction/10-top_score.sql
+++ 0x00-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
---lists all records of the specified table, also should be sorted in a descending order
+-- lists all records of the second_table, also should be sorted in a descending order
 SELECT score, name FROM second_table ORDER BY score DESC;
