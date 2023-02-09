73dd63ce35231fc33c88475e25fd85593d0c2f8c
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-09 03:10:33 -0600

    didn't specify what table to select[fixed]

--- 0x00-SQL_introduction/10-top_score.sql
+++ 0x00-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 --lists all records of the specified table, also should be sorted in a descending order
 
-SELECT score, name FROM seond table ORDER BY score DESC;
+SELECT score, name FROM seond_table ORDER BY score DESC;
