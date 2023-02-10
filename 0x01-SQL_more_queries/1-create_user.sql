223f414f261d915e653f2a12e29f12c96a0e9911
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-10 01:10:18 -0600

    added semicolon and changed AS to BY[fix]

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creating user, setting pass,  granting user
 
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED AS 'user_0d_1_pwd'
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
