536267c7032de1d1ad105aa7efc09485252255e9
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-09 21:56:04 -0600

    fixed typo didn't correctly specify which table[fix]

--- 0x00-SQL_introduction/16-no_link.sql
+++ 0x00-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- displaying data
 
-SELECT score, name FROM second_name WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
