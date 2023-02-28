1c85340ee2074acda636bbf07668d237b21ef7b8
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-28 14:53:40 -0600

    Update 5-filter_cities.py

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,18 @@
 #!/usr/bin/python3
-'''Get All States, requires User, Password, Database'''
-import MySQLdb
+"""
+List all cities of a state
+"""
 import sys
-
-
-def filter_states():
-    '''Get Cites and States from sql table'''
-    if(len(sys.argv) < 5):
-        print(sys.argv)
-        return
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    try:
-        db = MySQLdb.connect(
-            "localhost",
-            mysql_username,
-            mysql_password,
-            database_name)
-    except Exception as e:
-        return (0)
-    target = sys.argv[4]
-    target = target.split('"')[0]
-    target = target.split("'")[0]
-    cursor = db.cursor()
-    cursor.execute(
-        "SELECT \
-        (SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM\
-        cities WHERE cities.state_id =states.id), id\
-        FROM states WHERE states.name = '{}';".format(target))
-    rows = cursor.fetchall()
-    for r in rows:
-        print(r[0])
-    cursor.close()
-    db.close()
-
+import MySQLdb
 
 if __name__ == '__main__':
-    filter_states()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
+
+    print(", ".join([state[1] for state in states]))
