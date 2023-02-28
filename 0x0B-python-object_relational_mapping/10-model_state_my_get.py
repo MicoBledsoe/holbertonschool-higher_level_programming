aed8e5c5092a7fc00afbdacaa9dc80743eda8053
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-28 14:57:25 -0600

    Update 10-model_state_my_get.py

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""Fetch All States"""
+"""
+Prints the State object with the name passed as argument from the db
+"""
 import sys
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-def fetch_all_states():
-    '''Fetch All States'''
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-
-    target = sys.argv[4]
-    target = target.split('"')[0]
-    target = target.split("'")[0]
-
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(
-        State.name == (target)).first()
-    if(state is None):
-        print("Not Found")
-        return
-    print("{}".format(state.id))
-
-
-if __name__ == "__main__":
-    fetch_all_states()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    print("Not found" if not state else state.id)
