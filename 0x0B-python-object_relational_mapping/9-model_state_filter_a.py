fa6a1c3a3de000fdf2ab045d607846bf288fcbe9
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-28 14:56:31 -0600

    Update 9-model_state_filter_a.py

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Fetch All States"""
+"""
+Lists all State objects that contain the letter a from the database
+"""
 import sys
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-def fetch_all_states():
-    '''Fetch All States'''
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(
-     State.name.contains('a')).order_by(State.id).all():
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in states:
         print("{}: {}".format(state.id, state.name))
-
-
-if __name__ == "__main__":
-    fetch_all_states()
