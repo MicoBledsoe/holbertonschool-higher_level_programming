86fb0fc4594b3ea8bc1e31a303637b4871406767
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-28 15:09:58 -0600

    Update 12-model_state_update_id_2.py

--- 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""Fetch All States"""
+"""
+Changes the name of a State object from the database hbtn_0e_6_usa
+"""
 import sys
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-def insert_into_states():
-    '''Fetch All States'''
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = 'New Mexico'
-    session.commit()
+    stateUpdated = session.query(State).filter(State.id == 2).first()
 
-
-if __name__ == "__main__":
-    insert_into_states()
+    if stateUpdated:
+        stateUpdated.name = 'New Mexico'
+        session.commit()
