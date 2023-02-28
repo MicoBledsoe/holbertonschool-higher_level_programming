f3750a6c5e4c07e214a683604bb4f06a8dd97168
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-28 14:37:44 -0600

    Update model_state.py

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""File for First state model"""
-from sqlalchemy import Column, Integer, String
+"""State Class File"""
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
 class State(Base):
-    """class definition of a State and an instance Base"""
+    """State Class"""
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='state')
