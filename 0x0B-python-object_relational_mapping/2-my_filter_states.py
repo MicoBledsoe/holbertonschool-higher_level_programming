2722d9490df73ea2901005d31896ae1fad2c9567
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-28 14:59:56 -0600

    Update 2-my_filter_states.py

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
-'''Get All States, requires User, Password, Database'''
-import MySQLdb
+"""
+Lists all values in the states tables of a database where name
+matches the argument
+"""
 import sys
-
-
-def my_filter_states():
-    '''Get States from sql table'''
-    if(len(sys.argv) < 5):
-        return
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    try:
-        db = MySQLdb.connect(
-            "localhost",
-            mysql_username,
-            mysql_password,
-            database_name)
-    except Exception as e:
-        return (0)
-    cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states \
-        WHERE name LIKE '{}' ORDER BY id ASC;".format(sys.argv[4]))
-    rows = cursor.fetchall()
-    for r in rows:
-        print(r)
-    cursor.close()
-    db.close()
-
+import MySQLdb
 
 if __name__ == '__main__':
-    my_filter_states()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
