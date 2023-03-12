bff0aa306dfa8bafe94b3421dfc1fb2a399db123
MicoBledsoe <micomods@gmail.com>
2023-03-12 17:21:15 -0500

    fix

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -9,18 +9,9 @@ import sys
 
 if __name__ == '__main__':
     # Get the command line arguments
-    if len(sys.argv) != 3:
-        print("Usage: {} <username> <token>".format(sys.argv[0]))
-        sys.exit(1)
-    username = sys.argv[1]
-    token = sys.argv[2]
-
-    # Set up the request headers with Basic Authentication
+    username, password = sys.argv[1:]
     url = 'https://api.github.com/user'
-    headers = {'Authorization': 'Basic ' + username + ':' + token}
-
-    # Send the GET request
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, auth=(username, password))
 
     # Check the response status code
     if response.status_code != 200:
