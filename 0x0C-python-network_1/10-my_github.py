3e2fb5b88ad7a6f0e1e2c779e7a9cb89afa8d4ad
MicoBledsoe <micomods@gmail.com>
2023-03-12 17:25:11 -0500

    fix

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
+
 import requests
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
     username = sys.argv[1]
-    access_token = sys.argv[2]
-    url = f'https://api.github.com/user'
-    response = requests.get(url, auth=(username, access_token))
-    if response.status_code == 200:
-        print(response.json()['id'])
-    else:
+    token = sys.argv[2]
+    response = requests.get(url, auth=(username, token))
+    if response.status_code != 200:
         print("Error: Could not get user information")
-        print(response.text)
-        sys.exit(1)
+    else:
+        print(response.json()['id'])
