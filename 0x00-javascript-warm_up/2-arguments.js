313c072379697058d173bd844efba0a4e8c0e4eb
MicoBledsoe <micomods@gmail.com>
2023-04-07 16:20:38 -0500

    didnt assign them correctly

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const argsCount = process.argv.length;
+const argsCount = process.argv.length - 2;
 
-if (args === 2) {
+if (argsCount === 0) {
   console.log('No argument');
-} else if (args === 3) {
+} else if (argsCount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
