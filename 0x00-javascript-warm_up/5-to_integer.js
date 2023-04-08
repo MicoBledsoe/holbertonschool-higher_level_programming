35ee841f5d5275cae1b4e330a4e73d822f506d16
MicoBledsoe <micomods@gmail.com>
2023-04-07 21:26:40 -0500

    parsing

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const num = parseInt(process.argv[2]);
+const arg = process.argv[2];
 
-if (isNaN(num)) {
+if (!arg || isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${num}`);
+  console.log(`My number: ${parseInt(arg)}`);
 }
